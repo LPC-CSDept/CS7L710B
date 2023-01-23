289feb83e73e13d3d00eb78d1a6b0cb251f2b458
Kyuwoong Lee <kwooler@gmail.com>
2023-01-22 23:26:22 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+##################################################
+# Comlete your code here
+##################################################
+
+# numbers = list(map(int, input().split()))
+
+def main():
+    main.numbers = list(map(int, input().split()))
+
+    ith = int(input())
+
+    # Code your program here
+
+    print('The ith min val: ', main.ithmin)
+##
+
+
+if __name__ == '__main__':
+    main()
